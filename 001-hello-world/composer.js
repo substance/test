6ff54fa3958fe24e18842d834ac7e6b86a1f21b5
Michael Aufreiter <michael@substance.io>
2013-03-29 20:57:34 +0100

    First minimal test case.

---
+++ 001-hello-world/composer.js
@@ -0,0 +1,60 @@
+// Test data
+// ----------
+
+var c1 = {
+  "op": [
+    "insert",
+    {
+      "id": "heading:42c72d87e40f529dba27a9970c0a6ef3",
+      "type": "heading",
+      "data": {
+        "content": "Why should I care?"
+      }
+    }
+  ],
+  "sha": "b0a4df43adba704eaef6809ada25bc4a"
+};
+
+
+// Expose test case so test runner can find it
+
+window.test_case = [
+
+  // Create a new document
+  // ----------
+
+  function(cb) {
+    localStore.create('hello-world', function(err, doc) {
+      // assert.ok(doc.id === "hello-world");
+      cb(null, doc);
+    });
+  },
+
+  // Apply commit
+  // ----------
+  
+  function(doc, cb) {
+    var meta = {"keywords": ["hello", "world"]};
+    var refs = {
+      "master": "b0a4df43adba704eaef6809ada25bc4a",
+      "tail": "b0a4df43adba704eaef6809ada25bc4a"
+    };
+
+    // Apply commit
+    localStore.update("hello-world", [c1], meta, refs , function(err) {
+      cb(err);
+    });
+  },
+
+  // Check if commit has been stored
+  // ----------
+
+  function(arg1, callback) {
+    localStore.get("hello-world", function(err, doc) {
+      // assert(Object.keys(doc.commits).length === 1);
+      // assert(doc.refs.master === "b0a4df43adba704eaef6809ada25bc4a");
+      // assert(doc.refs.tail === "b0a4df43adba704eaef6809ada25bc4a");
+      cb(err, doc);
+    });
+  }
+];
