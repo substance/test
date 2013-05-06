b5e95671a06ce23d4f1f9b5a5be2c31c282bfca3
Oliver Buchtala <oliver.buchtala@googlemail.com>
2013-05-06 13:43:49 +0200

    Add a test addressing in blob replication.

---
+++ tests/replicator/009-update-blob.js
@@ -0,0 +1,80 @@
+var test = {};
+
+test.id = 'replicator-009-update-blob';
+test.name = 'Replicate an Updated Blob';
+test.category = 'Replicator';
+
+test.seeds = [
+  {
+    requires: "001-boilerplate",
+    local: "lorem_ipsum.json",
+    remote: "lorem_ipsum.json"
+  }
+];
+
+var INSERT_IMAGE = [
+      "insert",
+      {
+        "id": "image:1",
+        "type": "image",
+        "target": "back",
+        "data": {
+          "medium": "blob:1",
+          "large": "blob:1",
+          "caption": "A new image"
+        }
+      }
+     ];
+
+var UPDATED_IMAGE = [
+      "update",
+      {
+        "id": "image:1",
+        "data": {
+          "medium": "blob:2",
+          "large": "blob:2",
+          "caption": "Updated image"
+        }
+      }
+     ];
+
+test.actions = [
+  "Init the session", function(test, cb) {
+    session.authenticate("oliver", "abcd", cb);
+  },
+
+  "Initial replication", function(data, cb) {
+    session.replicate(this.proceed(data, cb));
+  },
+
+  "Load document", function(data, cb) {
+    session.loadDocument("lorem_ipsum", cb);
+  },
+
+  "Add a blob with commit locally", function(data, cb) {
+    session.createBlob("lorem_ipsum", "blob:1", "BASE64_BLOBDATA", function(err) {
+      if (err) return cb(err);
+      session.document.apply(INSERT_IMAGE);
+      cb(null);
+    });
+  },
+
+  "Replicate", function(data, cb) {
+    session.replicate(this.proceed(data, cb));
+  },
+
+  "Update the image", function(data, cb) {
+    session.createBlob("lorem_ipsum", "blob:2", "BASE64_BLOBDATA", function(err) {
+      if (err) return cb(err);
+      session.document.apply(UPDATED_IMAGE);
+      cb(null);
+    });
+  },
+
+  "Replicate", function(data, cb) {
+    session.replicate(cb);
+  },
+];
+
+Substance.registerTest(test);
+
