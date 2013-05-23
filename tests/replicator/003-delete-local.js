be2c6305cda85e3fc1d1b8111116e832bf25de53
Oliver Buchtala <oliver.buchtala@googlemail.com>
2013-05-23 15:51:29 +0200

    Generalized replicator tests.

--- tests/replicator/003-delete-local.js
+++ tests/replicator/003-delete-local.js
@@ -1,36 +1,39 @@
 (function(root) {
 
 var replicator = root.Substance.test.replicator;
-var test = new replicator.ReplicatorTest({local: true, remote:true});
 
-test.seeds = [{
-  requires: "001-boilerplate",
-  local: "lorem_ipsum.json",
-  remote: "lorem_ipsum.json"
-}];
+var DeleteLocal = function() {
 
-test.actions = [
-  "Login", replicator.ReplicatorTest.login("oliver", "abcd"),
+  this.seeds = [{
+    requires: "001-boilerplate",
+    local: "lorem_ipsum.json",
+    remote: "lorem_ipsum.json"
+  }];
 
-  "Initial replication", function(cb) {
-    this.session.replicate(cb);
-  },
+  this.actions = [
+    "Login", replicator.ReplicatorTest.login("oliver", "abcd"),
 
-  "Delete remote document", function(cb) {
-    this.remote.delete("lorem_ipsum", cb);
-  },
+    "Initial replication", function(cb) {
+      this.session.replicate(cb);
+    },
 
-  "Replicate", function(cb) {
-    this.session.replicate(cb);
-  },
+    "Delete remote document", function(cb) {
+      this.remote.delete("lorem_ipsum", cb);
+    },
 
-  "Now the document should have been removed locally", function() {
-    assert.isFalse(this.local.exists("lorem_ipsum"));
-  }
-];
+    "Replicate", function(cb) {
+      this.session.replicate(cb);
+    },
 
-replicator.DeleteLocal = test;
-root.Substance.registerTest(['Replicator', 'Delete Local'], test);
+    "Now the document should have been removed locally", function() {
+      assert.isFalse(this.local.exists("lorem_ipsum"));
+    }
+  ];
+}
+DeleteLocal.prototype = replicator.ReplicatorTest.prototype;
+
+replicator.DeleteLocal = DeleteLocal;
+root.Substance.registerTest(['Replicator', 'Delete Local'], new DeleteLocal());
 
 })(this);
 
--- tests/replicator/004-delete-remote.js
+++ tests/replicator/004-delete-remote.js
@@ -1,38 +1,41 @@
 (function(root) {
 
 var replicator = root.Substance.test.replicator;
-var test = new replicator.ReplicatorTest({local: true, remote:true});
 
-test.seeds = [{
-  requires: "001-boilerplate",
-  local: "lorem_ipsum.json",
-  remote: "lorem_ipsum.json"
-}];
+var DeleteRemote = function() {
 
-test.actions = [
-  "Login", replicator.ReplicatorTest.login("oliver", "abcd"),
+  this.seeds = [{
+    requires: "001-boilerplate",
+    local: "lorem_ipsum.json",
+    remote: "lorem_ipsum.json"
+  }];
 
-  "Initial replication", function(cb) {
-    this.session.replicate(cb);
-  },
+  this.actions = [
+    "Login", replicator.ReplicatorTest.login("oliver", "abcd"),
 
-  "Delete local document", function() {
-    this.local.delete("lorem_ipsum");
-  },
+    "Initial replication", function(cb) {
+      this.session.replicate(cb);
+    },
 
-  "Replicate", function(cb) {
-    this.session.replicate(cb);
-  },
+    "Delete local document", function() {
+      this.local.delete("lorem_ipsum");
+    },
 
-  "Now the document should have been removed remotely", function(cb) {
-    this.remote.exists("lorem_ipsum", function(err, exists) {
-      assert.isFalse(exists);
-      cb(null);
-    });
-  }
-];
+    "Replicate", function(cb) {
+      this.session.replicate(cb);
+    },
 
-replicator.DeleteRemote = test;
-root.Substance.registerTest(['Replicator', 'Delete Remote'], test);
+    "Now the document should have been removed remotely", function(cb) {
+      this.remote.exists("lorem_ipsum", function(err, exists) {
+        assert.isFalse(exists);
+        cb(null);
+      });
+    }
+  ];
+}
+DeleteRemote.prototype = replicator.ReplicatorTest.prototype;
+
+replicator.DeleteRemote = DeleteRemote;
+root.Substance.registerTest(['Replicator', 'Delete Remote'], new DeleteRemote());
 
 })(this);
