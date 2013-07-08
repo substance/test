2b65469b5393523b4f9044e2e76a0518c706e13d
Oliver Buchtala <oliver.buchtala@googlemail.com>
2013-07-08 03:04:05 +0200

    Show test-name in mocha.

--- src/mocha_adapter.js
+++ src/mocha_adapter.js
@@ -2,14 +2,15 @@
 
 // runs a given substance test in vows
 var registerTest = function(test) {
+  describe(test.path.join("/"), function() {
+    describe(test.name, function() {
+      test.setup();
 
-  describe(test.path.join("."), function() {
-    test.setup();
-
-    for (var idx = 0; idx < test.actions.length; idx++) {
-      var action = test.actions[idx];
-      it(action.label, action.func.bind(test));
-    }
+      for (var idx = 0; idx < test.actions.length; idx++) {
+        var action = test.actions[idx];
+        it(action.label, action.func.bind(test));
+      }
+    });
   });
 };
 
