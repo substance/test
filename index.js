78977dfc1a66481b30dbdf656f8de706a0f93834
Oliver Buchtala <oliver.buchtala@googlemail.com>
2013-07-19 19:40:25 +0200

    Test is also module.

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 "use strict";
 
 var Test = require('./src/test');
+Test.assert = require('./src/assert');
+Test.Runner = require('./src/controllers/test_runner');
+Test.MochaRunner = require('./src/controllers/mocha_test_runner');
+Test.View = {};
+Test.View.TestAction = require('./src/views/test_action');
+Test.View.TestCenter = require('./src/views/test_center');
+Test.View.TestReport = require('./src/views/test_report');
 
-module.exports = {
-  assert: require('./src/assert'),
-  Test: Test,
-  registerTest: Test.registerTest,
-  TestRunner: require('./src/controllers/test_runner'),
-  MochaTestRunner: require('./src/controllers/mocha_test_runner'),
-  TestAction: require('./src/views/test_action'),
-  TestCenter: require('./src/views/test_center'),
-  TestReport: require('./src/views/test_report')
-};
+module.exports = Test;
