5653603eaa656eb9519fd79d504eeeb92bc6c1b4
Andrey Popp <8mayday@gmail.com>
2013-08-23 00:48:08 +0400

    Fix Test.setup to execute on before with Mocha
    That way test.actions has access to effects of test.setup and do assertions
    against it.

--- src/controllers/mocha_test_runner.js
+++ src/controllers/mocha_test_runner.js
@@ -11,7 +11,7 @@ var MochaTestRunner = function() {
   var runTest = function(test) {
     describe(test.path.join("/"), function() {
       describe(test.name, function() {
-        it("Setup", test.setup.bind(test));
+        before(test.setup.bind(test));
         for (var idx = 0; idx < test.actions.length; idx++) {
           var action = test.actions[idx];
           it(action.label, action.func.bind(test));
