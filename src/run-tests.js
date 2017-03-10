f8cf8261529d4ac3c1b69114513469de22632ac4
Oliver Buchtala <oliver.buchtala@gmail.com>
2017-03-11 04:44:15 +1300

    Removed old run scripts.

--- src/run-tests.js
+++
@@ -1,4 +0,0 @@
-var run = require('substance-test/util/runner')
-var path = require('path')
-var testFile = path.join(__dirname, 'tests.cjs.js')
-run(testFile)
--- util/runner.js
+++
@@ -1,6 +0,0 @@
-// enabling source maps so that errors in bundler can be traced
-require('source-map-support').install()
-
-module.exports = function(pathToTests) {
-  require(pathToTests)
-}
