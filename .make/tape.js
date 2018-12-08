52ac43efd75195861f2065dd031a11e65b1ee4a2
Oliver Buchtala <oliver.buchtala@gmail.com>
2018-12-08 08:11:58 +0100

    Extend tape with isNil.

--- .make/tape.js
+++ .make/tape.js
@@ -1,4 +1,31 @@
 let tape = require('tape')
 let Test = require('tape/lib/test')
 tape.Test = Test
+
+function _isNil (obj) {
+  return o === null || o === undefined
+}
+
+Test.prototype.nil =
+Test.prototype.isNil = function (value, msg, extra) {
+  this._assert(_isNil(value), {
+    message: msg,
+    operator: 'nil',
+    expected: true,
+    actual: value,
+    extra: extra
+  })
+}
+
+Test.prototype.notNil =
+Test.prototype.isNotNil = function (value, msg, extra) {
+  this._assert(!_isNil(value), {
+    message: msg,
+    operator: 'nil',
+    expected: true,
+    actual: value,
+    extra: extra
+  })
+}
+
 module.exports = tape
