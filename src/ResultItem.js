ac989f81b64437f1c66e17e02fde35de3ec2d13c
Oliver Buchtala <oliver.buchtala@gmail.com>
2019-01-19 23:27:28 +0100

    Support t.comment()

--- src/ResultItem.js
+++ src/ResultItem.js
@@ -1,4 +1,4 @@
-import { substanceGlobals, Component, isArray, isPlainObject } from 'substance'
+import { substanceGlobals, Component, isArray, isPlainObject, isString } from 'substance'
 import DMP from '../vendor/google-diff-match-patch/dmp'
 
 substanceGlobals.DEBUG_RENDERING = false
@@ -15,43 +15,51 @@ export default class ResultItem extends Component {
   }
 
   render ($$) {
-    var test = this.props.test
-    var result = this.props.result
-    var el = $$('div').addClass('sc-test-result')
+    let test = this.props.test
+    let result = this.props.result
+    let el = $$('div').addClass('sc-test-result')
 
-    var header = $$('div')
-    if (!test._skip) {
-      if (result.ok) {
-        header.append($$('span').addClass('se-status sm-ok').append('\u2713'))
-      } else {
-        header.append($$('span').addClass('se-status sm-not-ok').append('\u26A0'))
+    if (isString(result)) {
+      el.addClass('sm-comment')
+      el.append(result)
+      // header.append(
+      //   $$('span').addClass('se-description').append(result)
+      // )
+    } else {
+      let header = $$('div')
+      if (!test._skip) {
+        if (result.ok) {
+          header.append($$('span').addClass('se-status sm-ok').append('\u2713'))
+        } else {
+          header.append($$('span').addClass('se-status sm-not-ok').append('\u26A0'))
+        }
       }
-    }
-    header.append($$('span').addClass('se-description').append(String(result.name)))
-    el.append(header)
+      header.append($$('span').addClass('se-description').append(String(result.name)))
+      el.append(header)
 
-    if (!test._skip && !result.ok && (result.expected || result.actual)) {
-      let expStr = _toString(result.expected)
-      let actStr = _toString(result.actual)
-      if (expStr !== actStr) {
-        el.append(this._renderPatch($$, expStr, actStr))
+      if (!test._skip && !result.ok && (result.expected || result.actual)) {
+        let expStr = _toString(result.expected)
+        let actStr = _toString(result.actual)
+        if (expStr !== actStr) {
+          el.append(this._renderPatch($$, expStr, actStr))
+        }
+        let diffEl = $$('div').addClass('se-diff')
+        diffEl.append(
+          $$('div').addClass('se-expected')
+            .append('Expected:')
+            .append(
+              $$('pre').append(_toString(result.expected))
+            )
+        )
+        diffEl.append(
+          $$('div').addClass('se-actual')
+            .append('Actual:')
+            .append(
+              $$('pre').append(_toString(result.actual))
+            )
+        )
+        el.append(diffEl)
       }
-      let diffEl = $$('div').addClass('se-diff')
-      diffEl.append(
-        $$('div').addClass('se-expected')
-          .append('Expected:')
-          .append(
-            $$('pre').append(_toString(result.expected))
-          )
-      )
-      diffEl.append(
-        $$('div').addClass('se-actual')
-          .append('Actual:')
-          .append(
-            $$('pre').append(_toString(result.actual))
-          )
-      )
-      el.append(diffEl)
     }
 
     return el
